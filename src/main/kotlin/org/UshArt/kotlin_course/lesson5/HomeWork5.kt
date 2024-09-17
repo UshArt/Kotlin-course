060cf50174124f6874f42238bef92b676f3bb311
UshArt007 <148473953+UshArt007@users.noreply.github.com>
2024-09-17 19:32:06 +0300

    Homework3

---
+++ src/main/kotlin/org/UshArt/kotlin_course/lesson5/HomeWork5.kt
@@ -0,0 +1,25 @@
+package org.UshArt.kotlin_course.lesson5
+
+fun main() {
+    val sum = 5 + 3
+    val diff = 10 - 3
+    val product = 10 * 5
+    val quotient = 10 / 5
+    val remainder = 10 % 5
+    val remainder1 = 10 % 6
+    val remainder2 = 10 % 4
+    val isEqual = (5 == 5)
+    val isNotEqual = (5!= 5)
+    val isGreater = (5 > 3)
+    val isLesser = (5 < 3)
+    val isGreaterOrEqual = (5 >= 5)
+    val isLesserOrEqual = (5 <= 3)
+    val andResult = false && false
+    val orResult = true || false
+    val notResult = !(5 > 3)
+    var number = 5
+    number += 3
+    number -= 2
+
+    println(sum)
+}
