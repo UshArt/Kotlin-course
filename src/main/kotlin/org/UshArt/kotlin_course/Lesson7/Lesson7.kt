a9da9f7cd88ba75eeb173df462537282184eb505
UshArt007 <148473953+UshArt007@users.noreply.github.com>
2024-09-24 21:07:38 +0300

    HomeWork6

---
+++ src/main/kotlin/org/UshArt/kotlin_course/Lesson7/Lesson7.kt
@@ -0,0 +1,64 @@
+package UshArt.kotlin_course.Lesson7
+
+//fun main() {
+//    for (i in 1..10) {
+//        println(i)
+//    }
+//}
+
+//fun main() {
+//    var counter = 10
+//    while (counter-- > 0) {
+//        println(counter)
+//    }
+//}
+
+//fun main() {
+//    do {
+//        println("+")
+//    } while (false)
+//}
+
+//fun main() {
+//    for (i in 1..10) {
+//        if (i == 2) continue
+//        if (i == 7)break
+//        println(i)
+//    }
+//}
+
+fun main() {
+//    for (i in 1..10) {
+//        println(i)
+//    }
+
+//    for (i in 1..5) {
+//        println(i * i)
+//    }
+
+//    for (i in 10 downTo 1) {
+//        println(i)
+//    }
+
+//    for (i in 20 downTo 2) {
+//        if (i % 2 == 0) {
+//            println(i)
+//        }
+//    }
+
+//    for (i in 1..30 step 3) {
+//        println(i)
+//    }
+
+//    var counter = 1
+//    var s = 0
+//    while (counter <= 10) {
+//        s = s + counter++
+//    }
+//    println(s)
+
+    var counter = 100
+    while (counter > 1) {
+        println(counter--.toString().length)
+    }
+}
