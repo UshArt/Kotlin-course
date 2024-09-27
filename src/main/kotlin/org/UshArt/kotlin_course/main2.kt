0e1336f8dc52137ac0203a75293b11662b0f23a7
UshArt007 <148473953+UshArt007@users.noreply.github.com>
2024-09-27 19:34:32 +0300

    Lesson5

--- src/main/kotlin/org/UshArt/kotlin_course/main2.kt
+++ src/main/kotlin/org/UshArt/kotlin_course/main2.kt
@@ -1,41 +1,41 @@
-val name: String = "Alice"
-
-var age: Int = 30
-
-const val PI = 3.14
-
-lateinit var userInfo: String
-
-val lazyValue: String by lazy {
-    "Hello, this is a lazy string!"
-}
-
-var speed: Double? = null
-
-var count: Int = 0
-    get() = field
-    set(value) {
-        if (value >=0) field = value
-    }
-
-
-val chassis: String = "12345ABC"
-
-var colour: String = "Blue"
-
-var distance: Double = 0.0
-
-lateinit var owner: String
-
-const val WHEELS = 4
-
-val lazyValue: String by lazy {
-    ""
-}
-
-var fuel: Double = 0.0
-    get () = field
-    set(value) {
-        if (value > 0) field += value
-    }
-
+//val name: String = "Alice"
+//
+//var age: Int = 30
+//
+//const val PI = 3.14
+//
+//lateinit var userInfo: String
+//
+//val lazyValue: String by lazy {
+//    "Hello, this is a lazy string!"
+//}
+//
+//var speed: Double? = null
+//
+//var count: Int = 0
+//    get() = field
+//    set(value) {
+//        if (value >=0) field = value
+//    }
+//
+//
+//val chassis: String = "12345ABC"
+//
+//var colour: String = "Blue"
+//
+//var distance: Double = 0.0
+//
+//lateinit var owner: String
+//
+//const val WHEELS = 4
+//
+//val lazyValue: String by lazy {
+//    ""
+//}
+//
+//var fuel: Double = 0.0
+//    get () = field
+//    set(value) {
+//        if (value > 0) field += value
+//    }
+//
